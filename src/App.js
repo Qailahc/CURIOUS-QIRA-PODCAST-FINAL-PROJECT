250ee804afacdd784c20f2bb41f42f2b3894b34c
Qailahc <149602288+Qailahc@users.noreply.github.com>
2024-06-18 14:05:10 +0200

    imported useState from react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import React, { useState } from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './utils/Themes';
+
+const Container = styled.div`
+  background: ${({ theme }) => theme.bg};
+  width: 100%;
+  height: 100vh;
+`;
+
 function App() {
+  // hooks
+  const [darkMode, setDarkMode] = useState(true);
+
   return (
-    <div>
-      Curious Qira Podcast
-    </div>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Container>Curious Qira Podcast</Container>
+    </ThemeProvider>
   );
 }
 
