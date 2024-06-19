cb47a78b39c3c2d20dbcfa64484895e298103b3f
Qailahc <149602288+Qailahc@users.noreply.github.com>
2024-06-19 13:39:41 +0200

    Updated theme primary colours

--- src/utils/Themes.js
+++ src/utils/Themes.js
@@ -1,7 +1,7 @@
 export const darkTheme = {
     bg:"#15171E",
     bgLight: "#1C1E27",
-    primary:"#95ECCA",
+    primary:"#98ddc2",
     text_primary:"#F2F3F4",
     text_secondary:"#b1b2b3",
     card:"#121212",
@@ -11,7 +11,7 @@ export const darkTheme = {
 export const lightTheme = {
     bg:"#FFFFFF",
     bgLight: "#f0f0f0",
-    primary:"#95ECCA",
+    primary:"#98ddc2",
     text_primary:"#111111",
     text_secondary:"#48494a",
     card:"#FFFFFF",
